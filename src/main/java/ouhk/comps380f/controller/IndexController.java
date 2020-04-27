8e0b5193ef83dbbf62d75820a9c23b9f1aa2bc31
iancng <iancng2@gmail.com>
2020-04-27 16:56:37 +0800

    Edit Topics

--- src/main/java/ouhk/comps380f/controller/IndexController.java
+++ src/main/java/ouhk/comps380f/controller/IndexController.java
@@ -2,22 +2,28 @@ package ouhk.comps380f.controller;
 
 import java.io.IOException;
 import java.security.Principal;
+import java.sql.Blob;
 import java.sql.Timestamp;
 import javax.annotation.Resource;
 import javax.swing.JOptionPane;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 import ouhk.comps380f.dao.PollRepository;
+import ouhk.comps380f.dao.TopicsRepository;
 import ouhk.comps380f.dao.UserRepository;
 import ouhk.comps380f.model.Poll;
+import ouhk.comps380f.model.Topics;
 import ouhk.comps380f.model.Users;
 import ouhk.comps380f.service.PollService;
 
 @Controller
 public class IndexController {
+    @Resource
+    private TopicsRepository topicsRepo;
     @Resource
     private PollRepository pollRepo;
     @Resource
@@ -44,16 +50,100 @@ public class IndexController {
         return "adminControl";
     }
     
-
-    @GetMapping("/admin/thread")
-    public String manageThread(){
-        return "manageThread";
+    @GetMapping("/admin/topics")
+    public String manageTopics(ModelMap model){
+        model.addAttribute("topics", topicsRepo.findAll());
+        return "manageTopics";
     }
     
-     @GetMapping("/admin/createPoll")
+    
+    @GetMapping("/admin/editTopics/{topicID}")
+    public ModelAndView editTopics(@PathVariable("topicID") Long topicID, ModelMap model){
+        model.addAttribute("topic",topicsRepo.findById(topicID).orElse(null));
+        return new ModelAndView("editTopics", "topicForm", new topicForm());
+    }
+    
+    
+    @PostMapping("/admin/editTopics/{topicID}")
+    public String editTopicsPost(@PathVariable("topicID")Long topicID, topicForm form,Principal principal){
+        Topics editTopic = topicsRepo.findById(topicID).orElse(null);
+        editTopic.setTitle(form.title);
+        editTopic.setContent(form.content);
+        topicsRepo.save(editTopic);
+        return "redirect:/admin/topics";
+    }
+    public static class topicForm{
+        private String title;
+        private String content;
+        private String author;
+        private Timestamp time;
+        private String filename;
+        private Blob file;
+        private String categories;
+
+        public String getTitle() {
+            return title;
+        }
+
+        public void setTitle(String title) {
+            this.title = title;
+        }
+
+        public String getContent() {
+            return content;
+        }
+
+        public void setContent(String content) {
+            this.content = content;
+        }
+
+        public String getAuthor() {
+            return author;
+        }
+
+        public void setAuthor(String author) {
+            this.author = author;
+        }
+
+        public Timestamp getTime() {
+            return time;
+        }
+
+        public void setTime(Timestamp time) {
+            this.time = time;
+        }
+
+        public String getFilename() {
+            return filename;
+        }
+
+        public void setFilename(String filename) {
+            this.filename = filename;
+        }
+
+        public Blob getFile() {
+            return file;
+        }
+
+        public void setFile(Blob file) {
+            this.file = file;
+        }
+
+        public String getCategories() {
+            return categories;
+        }
+
+        public void setCategories(String categories) {
+            this.categories = categories;
+        }
+        
+    }
+    
+    @GetMapping("/admin/createPoll")
     public ModelAndView createPoll(){
         return new ModelAndView("createPoll", "createPollForm", new createPollForm());
     }
+    
     @PostMapping("/admin/createPoll")
     public String createPoll(createPollForm form,Principal principal) throws IOException{
         Poll newPoll = new Poll();
